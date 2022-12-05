d3328d10e1e0bc93afc4b54b0a9b9da84b946c6f
sannarino <einar.rahka@gmail.com>
2022-12-05 19:11:24 +0100

    First commit

---
+++ AOC22/Day1.cs
@@ -0,0 +1,21 @@
+namespace AOC22;
+
+public class Day1
+{
+    public static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("C:\\Users\\33448\\Desktop\\Programming\\AOC22\\AOC22\\Day1_input.txt");
+
+        var sum = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            
+            Convert.ToInt32(input[i]);
+            Console.WriteLine(input[i]);
+            if(input[i] != "")
+                
+                
+        }
+        Console.WriteLine(sum);
+    }
+}
